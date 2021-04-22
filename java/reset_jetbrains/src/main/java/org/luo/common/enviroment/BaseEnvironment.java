058bab376d785e206e7934d328a06714bc771e67
wangzb <wg1083107883@gmail.com>
2021-04-22 21:06:58 +0800

    添加Jetbrains产品无限试用工具

---
+++ java/reset_jetbrains/src/main/java/org/luo/common/enviroment/BaseEnvironment.java
@@ -0,0 +1,10 @@
+package org.luo.common.enviroment;
+
+public class BaseEnvironment {
+    /**
+     * jetbrains
+     */
+    public static final String[] JETBRAIN_CONFIG_PATH = new String[]{"AppData", "Roaming", "JetBrains"};
+
+    public static final String[] JETBRAINS_PRODUCE_LIST = new String[]{"IntelliJIdea", "GoLand", "CLion", "PyCharm", "DataGrip", "RubyMine", "AppCode", "PhpStorm", "WebStorm", "Rider"};
+}
---
+++ java/reset_jetbrains/src/main/java/org/luo/common/util/PropertiesUtil.java
@@ -0,0 +1,32 @@
+package org.luo.common.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+    private static Properties props =  null;
+
+    public PropertiesUtil(String path){
+        new PropertiesUtil(new File(path));
+    }
+
+    public PropertiesUtil(File properties){
+        try(FileInputStream inputStream = new FileInputStream(properties)){
+            props = new Properties();
+            props.load(inputStream);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.err.println("未找到指定配置文件");
+            System.exit(-1);
+        }
+    }
+
+    public String get(String key) {
+        return props.getProperty(key, null);
+    }
+
+
+
+
+}
