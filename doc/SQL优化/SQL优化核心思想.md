1ef479580098fc06c69f4a909f72ea021ead6a09
wangzb <wzbao76505@163.com>
2022-04-17 16:46:55 +0800

    feat:上传sql优化笔记

--- doc/SQL优化/SQL优化核心思想.md
+++ doc/SQL优化/SQL优化核心思想.md
@@ -1,6 +1,21 @@
+[TOC]
+
+
+
 ## 1、必懂概念
 
 ### 1.1、基数(Cardinality)
 
-> 某个列唯一键(Distinct_Keys)的数量叫做基数。
+> 某个列唯一键(Distinct_Keys)的数量叫做基数。也可称为数据分布。
 
+```java
+System.out.println("hello world!!!");
+```
+
+### 1.2、选择性(Selectivity)
+
+> 基数与总行数的的比值再乘以100%就是某个列的选择性。
+
+
+
+å
--- doc/SQL优化/sql记录.sql
+++ doc/SQL优化/sql记录.sql
@@ -1,30 +1,35 @@
-
-
 --创建test表
-create table test as select * from DBA_OBJECTS;
+create table test as
+select *
+from DBA_OBJECTS;
 
 --查看test表owner列和object_id列的基数
-select count(distinct OWNER) ,count(distinct OBJECT_ID),count(*) from test;
+select count(distinct OWNER), count(distinct OBJECT_ID), count(*)
+from test;
 
 --查看test表owner列的数据分布
-select owner,count(*) from test group by owner order by 2 desc;
+select owner, count(*)
+from test
+group by owner
+order by 2 desc;
 
 --收集test表的统计信息
 begin
     dbms_stats.GATHER_TABLE_STATS(ownname => 'SCOTT',
-                    tabname => 'test',
-                    estimate_percent => 100,
-                    method_opt => 'for all columns size 1',
-                    no_invalidate => FALSE,
-                    degree => 1,
-                    cascade => TRUE
+                                  tabname => 'test',
+                                  estimate_percent => 100,
+                                  method_opt => 'for all columns size 1',
+                                  no_invalidate => FALSE,
+                                  degree => 1,
+                                  cascade => TRUE
         );
 end;
 /
---查看test表中每个列的基数与选择性
-select a.column_name as "列明",
-       b.num_rows as "行数",
-       a.num_distinct  as "基数",  --                          Cardinality,
+
+--查看数据库表中每个列的基数与选择性，以scott用户下的test表为例
+select a.column_name                               as "列明",
+       b.num_rows                                  as "行数",
+       a.num_distinct                              as "基数", --                          Cardinality,
        round(a.num_distinct / b.num_rows * 100, 2) as "选择性",--selectivity,
        a.histogram,
        a.num_buckets
@@ -34,4 +39,6 @@ where a.OWNER = b.OWNER
   and a.TABLE_NAME = b.TABLE_NAME
   and a.OWNER = 'SCOTT'
   and a.TABLE_NAME = 'TEST';
+
+
 
