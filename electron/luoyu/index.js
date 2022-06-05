9be3738ba09ce8d3fff022832b939f8329cc02a7
wangzb <wzbao76505@163.com>
2022-06-05 20:03:09 +0800

    feat:上传electron代码

---
+++ electron/luoyu/index.js
@@ -0,0 +1,25 @@
+var electron = require('electron');
+//整个应用，可以获取应用程序生命周期中的各个事件
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var win = null;
+//解决“Electron Security Warning”安全警告
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+app.on('ready', function () {
+    win = new BrowserWindow({
+        //配置对象
+        webPreferences: {
+            //页面集成node.js,并赋予index.html页面中的javascript访问node环境，访问互联网页面并无法验证是否安全，则该项应为false
+            nodeIntegration: true
+        }
+    });
+    win.loadFile("index.html");
+    //打开开发者工具
+    win.webContents.openDevTools();
+    win.on('closed', function () {
+        win = null;
+    })
+})
+app.on('window-all-closed', function () {
+    app.quit();
+})
